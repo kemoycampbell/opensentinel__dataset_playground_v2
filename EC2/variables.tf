107761b44a92e365c5fa7aa7bee5d90b66e6a460
Kemoy Campbell <kemoycampbell45@gmail.com>
2026-04-26 13:55:03 -0400

    add running images and change to t3.micro

--- EC2/variables.tf
+++ EC2/variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 }
 
 variable "instance_type" {
-  default     = "t2.micro"
+  default     = "t3.micro"
   type        = string
   description = "The type of EC2 instance to use."
 }
